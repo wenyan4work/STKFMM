8c568dd17914f020e23e5599507a434a80130c99
wenyan4work <wenyan4work@gmail.com>
2020-03-19 16:12:56 -0400

    add PVel M2L3D

--- M2L/StokesPVel/main.cpp
+++ M2L/StokesPVel/main.cpp
@@ -8,9 +8,9 @@ namespace StokesPVel2D3D {
 int main(int argc, char *argv[]);
 }
 
-// namespace StokesPVel3D3D {
-// int main(int argc, char *argv[]);
-// }
+namespace StokesPVel3D3D {
+int main(int argc, char *argv[]);
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
@@ -28,9 +28,9 @@ int main(int argc, char *argv[]) {
     case 2:
         StokesPVel2D3D::main(argc, argv);
         break;
-    // case 3:
-    //     Stokes3D3D::main(argc, argv);
-    //     break;
+    case 3:
+        StokesPVel3D3D::main(argc, argv);
+        break;
     default:
         std::cerr << "Invalid dimension {" << dim << "}.\n";
         return 1;
