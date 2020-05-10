d97a593e91b219030f19fcc4605ba94044679083
wenyan4work <wenyan4work@gmail.com>
2020-05-09 23:30:57 -0400

    add configuration switches

--- do-cmake.sh
+++ do-cmake.sh
@@ -9,7 +9,9 @@ cmake \
   -D CMAKE_BUILD_TYPE=Release \
   -D Eigen3_DIR="${SFTPATH}/share/eigen3/cmake" \
   -D CMAKE_INSTALL_PREFIX=${SFTPATH} \
+  -D BUILD_TEST=ON \
+  -D BUILD_DOC=OFF \
+  -D BUILD_M2L=OFF \
   -D PyInterface=OFF \
-  -D BUILD_DOC=ON \
 ../
 
