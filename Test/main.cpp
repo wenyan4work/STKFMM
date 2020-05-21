65e27e6907150455f18cbd58853a88a55943aa1a
wenyan4work <wenyan4work@gmail.com>
2020-05-21 12:07:26 -0400

    change trans_point generation

--- Test/main.cpp
+++ Test/main.cpp
@@ -12,14 +12,19 @@ int main(int argc, char **argv) {
     int myRank;
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 
-    Point point;
+    Point point, trans_point;
     Input input;
     Result pResult, verifyResult, convResult, transResult;
     Timing timing;
     std::vector<Record> history;
 
     genPoint(config, point, 3);
+
     genSrcValue(config, point, input);
+    if(config.pbc){
+        trans_point=point;
+            translatePoint(config, trans_point);
+    }
 
     printf_rank0("src value generated\n");
 
@@ -52,23 +57,19 @@ int main(int argc, char **argv) {
     }
 
     for (int p = 6; p < maxP; p += 2) {
+        printf_rank0("*********Testing order p = %d*********\n", p);
         pResult.clear();
         transResult.clear();
         timing.clear();
 
         runFMM(config, p, point, input, pResult, timing);
         dumpValue("p" + std::to_string(p), point, input, pResult);
-        printf_rank0("*********Testing order p = %d*********\n", p);
 
         if (config.pbc) {
-            Point trans_point = point;
             Timing transTiming;
-            translatePoint(config, trans_point);
             runFMM(config, p, trans_point, input, transResult, transTiming);
             dumpValue("trans_p" + std::to_string(p), trans_point, input, transResult);
         }
-
-        printf_rank0("------------------------------------\n");
 
         appendHistory(history, p, timing, pResult, verifyResult, convResult, transResult);
     }
