93795e7bf299d0e7b4d1fde4c5898f063b8f5aed
Robert Blackwell <rblackwell@scclin016.flatironinstitute.org>
2021-10-25 15:16:05 -0400

    Add new LaplaceLayerKernel::QPGradGrad()

--- Lib/include/STKFMM/LaplaceLayerKernel.hpp
+++ Lib/include/STKFMM/LaplaceLayerKernel.hpp
@@ -230,6 +230,121 @@ struct laplace_dipolepgradgrad_new : public GenericKernel<laplace_dipolepgradgra
     }
 };
 
+struct laplace_quadp_new : public GenericKernel<laplace_quadp_new> {
+    static const int FLOPS = 20;
+    template <class Real>
+    static Real ScaleFactor() {
+        return 1.0 / (4.0 * const_pi<Real>());
+    }
+    template <class VecType, int digits>
+    static void uKerEval(VecType (&u)[1], const VecType (&r)[3], const VecType (&f)[9], const void *ctx_ptr) {
+        VecType r2 = r[0] * r[0] + r[1] * r[1] + r[2] * r[2];
+        VecType rinv = sctl::approx_rsqrt<digits>(r2, r2 > VecType::Zero());
+        VecType rinv5 = rinv * rinv;
+        rinv5 = rinv5 * rinv5 * rinv;
+
+        // clang-format off
+        const VecType &sxx = f[0], &sxy = f[1], &sxz = f[2];
+        const VecType &syx = f[3], &syy = f[4], &syz = f[5];
+        const VecType &szx = f[6], &szy = f[7], &szz = f[8];
+        const VecType &dx = r[0], &dy = r[1], &dz = r[2];
+        // clang-format on
+
+        VecType commonCoeff = f[0] * dx * dx;
+        commonCoeff += (sxy + syx) * dx * dy;
+        commonCoeff += (sxz + szx) * dx * dz;
+        commonCoeff += syy * dy * dy;
+        commonCoeff += (syz + szy) * dy * dz;
+        commonCoeff += szz * dz * dz;
+        commonCoeff *= (typename VecType::ScalarType)(-3.0);
+
+        u[0] += (commonCoeff + r2 * (sxx + syy + szz)) * rinv5;
+    }
+};
+
+
+struct laplace_quadpgradgrad_new : public GenericKernel<laplace_quadpgradgrad_new> {
+    static const int FLOPS = 20;
+    template <class Real>
+    static Real ScaleFactor() {
+        return 1.0 / (4.0 * const_pi<Real>());
+    }
+    template <class VecType, int digits>
+    static void uKerEval(VecType (&u)[10], const VecType (&r)[3], const VecType (&f)[9], const void *ctx_ptr) {
+        VecType r2 = r[0] * r[0] + r[1] * r[1] + r[2] * r[2];
+        VecType rinv = sctl::approx_rsqrt<digits>(r2, r2 > VecType::Zero());
+        VecType rinv2 = rinv * rinv;
+        VecType rinv3 = rinv2 * rinv;
+        VecType rinv5 = rinv3 * rinv2;
+        VecType rinv7 = rinv5 * rinv2;
+        VecType rinv9 = rinv7 * rinv2;
+        const VecType two = (typename VecType::ScalarType)(2.0);
+        const VecType three = (typename VecType::ScalarType)(3.0);
+        const VecType five = (typename VecType::ScalarType)(5.0);
+        const VecType fifteen = (typename VecType::ScalarType)(15.0);
+        const VecType onezerofive = (typename VecType::ScalarType)(105.0);
+
+        // clang-format off
+        const VecType &sxx = f[0], &sxy = f[1], &sxz = f[2];
+        const VecType &syx = f[3], &syy = f[4], &syz = f[5];
+        const VecType &szx = f[6], &szy = f[7], &szz = f[8];
+        const VecType &dx = r[0], &dy = r[1], &dz = r[2];
+        // clang-format on
+
+        VecType rrQ = dx * dx * sxx + dx * dy * (sxy + syx) + dx * dz * (sxz + szx) + dy * dy * syy +
+                      dy * dz * (syz + szy) + dz * dz * szz;
+
+        VecType commonCoeff3 = rrQ * three;
+        VecType commonCoeff5 = rrQ * five;
+
+        VecType trace = sxx + syy + szz;
+
+        VecType rksxk = dx * sxx + dy * sxy + dz * sxz;
+        VecType rksyk = dx * syx + dy * syy + dz * syz;
+        VecType rkszk = dx * szx + dy * szy + dz * szz;
+
+        VecType rkskx = dx * sxx + dy * syx + dz * szx;
+        VecType rksky = dx * sxy + dy * syy + dz * szy;
+        VecType rkskz = dx * sxz + dy * syz + dz * szz;
+
+
+        u[0] += (commonCoeff3 - r2 * trace) * rinv5; //p
+        VecType px = dx * commonCoeff5 - r2 * (rksxk + rkskx + dx * trace);
+        VecType py = dy * commonCoeff5 - r2 * (rksyk + rksky + dy * trace);
+        VecType pz = dz * commonCoeff5 - r2 * (rkszk + rkskz + dz * trace);
+
+        u[1] -= three * px * rinv7; // gx
+        u[2] -= three * py * rinv7; // gy
+        u[3] -= three * pz * rinv7; // gz
+
+        // gxx
+        u[4] +=
+            rinv9 * (onezerofive * dx * dx * rrQ - r2 * fifteen * (rrQ + dx * two * (rksxk + rkskx) + dx * dx * trace) +
+                     three * r2 * r2 * (trace + two * sxx));
+        // gxy
+        u[5] += rinv9 * (onezerofive * (dx * dy * rrQ) -
+                         r2 * fifteen * ((dy * rksxk + dy * rkskx + dx * rksyk + dx * rksky) + dx * dy * trace) +
+                         three * r2 * r2 * (sxy + syx));
+        // gxz
+        u[6] += rinv9 * (onezerofive * (dx * dz * rrQ) -
+                         r2 * fifteen * ((dz * rksxk + dz * rkskx + dx * rkszk + dx * rkskz) + dx * dz * trace) +
+                         three * r2 * r2 * (sxz + szx));
+        // gyy
+        u[7] +=
+            rinv9 * (onezerofive * dy * dy * rrQ - r2 * fifteen * (rrQ + dy * two * (rksyk + rksky) + dy * dy * trace) +
+                     three * r2 * r2 * (trace + two * syy));
+        // gyz
+        u[8] += rinv9 * (onezerofive * (dy * dz * rrQ) -
+                         r2 * fifteen * ((dy * rkszk + dy * rkskz + dz * rksyk + dz * rksky) + dy * dz * trace) +
+                         three * r2 * r2 * (syz + szy));
+        // gzz
+        u[9] +=
+            rinv9 * (onezerofive * dz * dz * rrQ - r2 * fifteen * (rrQ + dz * two * (rkszk + rkskz) + dz * dz * trace) +
+                     three * r2 * r2 * (trace + two * szz));
+    }
+};
+
+
 template<class T> const Kernel<T>& LaplaceLayerKernelNew<T>::PGrad(){
   static Kernel<T> lap_pker =
       BuildKernel<T, laplace_p_new::Eval<T>, laplace_dipolep_new::Eval<T>>("laplace", 3, std::pair<int, int>(1, 1));
@@ -254,6 +369,21 @@ inline const Kernel<T> &LaplaceLayerKernelNew<T>::PGradGrad() {
         "laplace_PGradGrad", 3, std::pair<int, int>(1, 10), &lap_pker, &lap_pker, NULL, &lap_pker, &lap_pker, NULL,
         &lap_pker, NULL);
     lap_pgker.surf_dim = 3;
+
+    return lap_pgker;
+}
+
+template <class T>
+inline const Kernel<T> &LaplaceLayerKernelNew<T>::QPGradGrad() {
+    static Kernel<T> lap_pker = BuildKernel<T, laplace_p_new::Eval<T>>("laplace", 3, std::pair<int, int>(1, 1));
+    static Kernel<T> lap_pggker =
+        BuildKernel<T, laplace_pgradgrad_new::Eval<T>>("laplace", 3, std::pair<int, int>(1, 10));
+    static Kernel<T> lap_qpker = BuildKernel<T, laplace_quadp_new::Eval<T>>("laplace", 3, std::pair<int, int>(9, 1));
+
+    static Kernel<T> lap_pgker = BuildKernel<T, laplace_quadpgradgrad_new::Eval<T>>(
+        "laplace_QPGradGrad", 3, std::pair<int, int>(9, 10), &lap_qpker, &lap_qpker, NULL, //
+        &lap_pker, &lap_pker, &lap_pggker,                                                 //
+        &lap_pker, &lap_pggker);
 
     return lap_pgker;
 }
